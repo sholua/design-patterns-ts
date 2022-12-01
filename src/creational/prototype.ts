30460c3439bcdfaa55816021f3fac76e958ff4c3
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-12-01 16:29:21 +0200

    Prototype

---
+++ src/creational/prototype.ts
@@ -0,0 +1,13 @@
+// Prototype allows objects to be clones of other objects, rather then extended via inheritance.
+const zombie = {
+  eatBrains() {
+    return 'yum...';
+  }
+}
+
+const chad = Object.create(zombie, { name: { value: 'chad'} });
+
+console.log(chad.__proto__);
+console.log(Object.getPrototypeOf(chad));
+
+const babyChad = Object.create(chad, { baby: { value: true } });
